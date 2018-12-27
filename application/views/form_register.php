be1ce152b58504d974e43efdbe525053785ba777
thanapatwongwean <thanapatwongwean@gmail.com>
2018-12-28 01:44:20 +0700

    update register 2

--- application/views/form_register.php
+++ application/views/form_register.php
@@ -7,59 +7,66 @@
     <p>Let's join us to try or get some cool gaming gadget or IT stuff!</p>
     <?php echo validation_errors('<div>', '</div>'); ?>
     <?= form_open('register', 'class=""'); ?>
-    <div>
-        <?php
-        echo form_label('Username:', 'username');
-        $data = ['name' => 'username', 'id' => 'username','cols' => '50','rows' => '1'];
-        echo form_textarea($data);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo form_label('Password:', 'password');
-        $data = ['name' => 'password', 'id' => 'password','cols' => '50','rows' => '1'];
-        echo form_password($data);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo form_label('Confirm Password:', 'password');
-        $data = ['name' => 'passwordConf', 'id' => 'passwordConf','cols' => '50','rows' => '1'];
-        echo form_password($data);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo form_label('Fullname:', 'fullname');
-        $data = ['name' => 'fullname', 'id' => 'fullname','cols' => '50','rows' => '1'];
-        echo form_textarea($data);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo form_label('Address:', 'address');
-        $data = ['name' => 'address', 'id' => 'address','cols' => '50','rows' => '3'];
-        echo form_textarea($data);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo form_label('Phone:', 'phone');
-        $data = ['name' => 'phone', 'id' => 'phone','cols' => '50','rows' => '1'];
-        echo form_textarea($data);
-        ?>
-    </div>
-    <div>
-        <?php
-        echo form_label('Email:', 'email');
-        $data = ['name' => 'email', 'id' => 'email','cols' => '50','rows' => '1'];
-        echo form_textarea($data);
-        ?>
-    </div>
-    <div>
-        <?= form_submit('submit', 'Submit'); ?>
-        <?= form_reset('reset', 'Reset'); ?>
-    </div>
+        <div class="form-group">
+            <?php
+            echo form_label('Username', 'username');
+            echo '<br>';
+            $data = ['name' => 'username', 'id' => 'username', 'size' => '50' ,'class' => 'form-control'];
+            echo form_input($data);
+            ?>
+         </div>
+        <div class="form-group">
+            <?php
+            echo form_label('Password', 'password');
+            echo '<br>';
+            $data = ['name' => 'password', 'id' => 'password', 'size' => '50' ,'class' => 'form-control'];
+            echo form_password($data);
+            ?>
+        </div>
+        <div class="form-group">
+            <?php
+            echo form_label('Confirm Password', 'password');
+            echo '<br>';
+            $data = ['name' => 'passwordConf', 'id' => 'passwordConf','size' => '50' ,'class' => 'form-control'];
+            echo form_password($data);
+            ?>
+            </div>
+        <div>
+            <?php
+            echo form_label('Fullname', 'fullname');
+            echo '<br>';
+            $data = ['name' => 'fullname', 'id' => 'fullname','size' => '50' ,'class' => 'form-control'];
+            echo form_input($data);
+            ?>
+        </div>
+        <div class="form-group">
+            <?php
+            echo form_label('Address', 'address');
+            echo '<br>';
+            $data = ['name' => 'address', 'id' => 'address','cols' => '50','rows' => '3','class' => 'form-control'];
+            echo form_textarea($data);
+            ?>
+        </div>
+        <div class="form-group">
+            <?php
+            echo form_label('Phone', 'phone');
+            echo '<br>';
+            $data = ['name' => 'phone', 'id' => 'phone','size' => '50' ,'class' => 'form-control'];
+            echo form_input($data);
+            ?>
+        </div>
+        <div class="form-group">
+            <?php
+            echo form_label('Email','email');
+            echo '<br>';
+            $data = ['name' => 'email', 'id' => 'email','size' => '50','class' => 'form-control'];
+            echo form_input($data);
+            ?>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary"><i class="fa fa-user-md"></i>Submit</button>
+            <button type="reset" class="btn btn-primary">Reset</button>
+        </div>
 
     <?= form_close(); ?>
 </div>
@@ -67,7 +74,7 @@
 </main>
 <!-- /CONTENT -->
 
-<div class="col-lg-6">
+<!--<div class="col-lg-6">
 	<div class="box">
 test css
 	<div class="form-group">
@@ -78,4 +85,4 @@ test css
         <button type="submit" class="btn btn-primary"><i class="fa fa-user-md"></i>Submit</button>
     </div>
 </div>
-</div>
+</div>--!>
