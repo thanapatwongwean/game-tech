07bd4d70d61dad573310a0612e11acbdf25f7795
thanapatwongwean <thanapatwongwean@gmail.com>
2018-12-28 02:15:57 +0700

    add check_user function in user_model

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,5 +7,18 @@ class User_model extends CI_Model{
         return $this->db->insert('post', $data);
     }
 
+    public function check_user($email,$password){
+        $this->db->where('email',$email);
+        $datas = $this->db->get('email');
+        if ($datas->num_rows() > 0) {
+            $data = $datas->row();
+            $hash = $data->password;
+            if (password_verify($password, $hash)) {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
 
 }
