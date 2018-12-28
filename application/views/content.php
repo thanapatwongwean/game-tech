48eb1534ee7ee5dbc3736faa4e3941d766aec9eb
PheeHandy <PheeHandy@gmail.com>
2018-12-28 19:40:26 +0700

    Update content.php
    Add grid layer to content

--- application/views/content.php
+++ application/views/content.php
@@ -2,7 +2,16 @@
     *** CONTENT ***
     _________________________________________________________
     -->
-<div id="content">
+	
+
+
+       
+	   
+<div class="grid-container">
+  <div class="maintop">1</div>
+  <div class="mainleft">2</div>
+  <div class="mainright">
+  <div id="content">
         <div class="container">
           <div class="row">
             <div class="col-md-12">
@@ -90,9 +99,9 @@
     
     <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1" transform="translate(0,-459.36219)">
         <rect style="fill:#399f8b;fill-opacity:1;stroke:none" id="rect3336" width="10" height="50" x="0" y="481.36218"></rect>
-        <text xml:space="preserve" style="font-style:normal;font-weight:normal;font-size:40px;line-height:125%;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" x="19.978516" y="519.68359" id="text4148" sodipodi:linespacing="125%"><tspan sodipodi:role="line" x="19.978516" y="519.68359" id="tspan4150" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:condensed;font-size:36px;line-height:125%;font-family:'agency-fb';-inkscape-font-specification:'Agency FB Bold Condensed';text-align:start;writing-mode:lr-tb;text-anchor:start"><tspan x="19.978516" y="519.68359" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:condensed;font-size:36px;line-height:125%;font-family:'agency-fb';-inkscape-font-specification:'Agency FB Bold Condensed';text-align:start;writing-mode:lr-tb;text-anchor:start" id="tspan4152"> / </tspan></tspan></text>
+        <text xml:space="preserve" style="font-style:normal;font-weight:normal;font-size:40px;line-height:125%;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" x="19.978516" y="519.68359" id="text4148" sodipodi:linespacing="125%"><tspan sodipodi:role="line" x="19.978516" y="519.68359" id="tspan4150" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:condensed;font-size:36px;line-height:125%;font-family:'agency-fb';-inkscape-font-specification:'Agency FB Bold Condensed';text-align:start;writing-mode:lr-tb;text-anchor:start"></tspan></text>
     <text xml:space="preserve" style="font-style:normal;font-weight:normal;font-size:40px;line-height:125%;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" x="20.142344" y="559.79712" id="text4148-9" sodipodi:linespacing="125%"><tspan sodipodi:role="line" x="20.142344" y="559.79712" id="tspan4153" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:condensed;font-size:30px;font-family:'agency-fb';-inkscape-font-specification:'Agency FB Bold Condensed'">0 BAHT</tspan></text>
-        <path sodipodi:type="star" style="fill:#333333;fill-opacity:1;stroke:none" id="path4188" sodipodi:sides="3" sodipodi:cx="863.68042" sodipodi:cy="395.76303" sodipodi:r1="84.575775" sodipodi:r2="42.287888" sodipodi:arg1="-0.51173884" sodipodi:arg2="0.53545871" inkscape:flatsided="true" inkscape:rounded="0" inkscape:randomized="0" d="M 937.42156,354.34678 862.67738,480.33286 790.94232,352.60946 Z" inkscape:transform-center-x="2.4118798" inkscape:transform-center-y="0.44436088" transform="matrix(-0.72157705,-0.71184898,0.40065019,-0.41955747,1191.19,1275.3531)"></path>
+        <path sodipodi:type="star" style="fill:#444444;fill-opacity:1;stroke:none" id="path4188" sodipodi:sides="3" sodipodi:cx="863.68042" sodipodi:cy="395.76303" sodipodi:r1="84.575775" sodipodi:r2="42.287888" sodipodi:arg1="-0.51173884" sodipodi:arg2="0.53545871" inkscape:flatsided="true" inkscape:rounded="0" inkscape:randomized="0" d="M 937.42156,354.34678 862.67738,480.33286 790.94232,352.60946 Z" inkscape:transform-center-x="2.4118798" inkscape:transform-center-y="0.44436088" transform="matrix(-0.72157705,-0.71184898,0.40065019,-0.41955747,1191.19,1275.3531)"></path>
         <g id="part-case">
             <path transform="matrix(2.5253911,-0.02098513,0.02098513,2.5253911,-90.965257,-907.96176)" d="m 215.67102,677.85408 -31.61616,17.77797 -31.20425,-18.49141 0.4119,-36.26938 31.61616,-17.77797 31.20425,18.49141 z" inkscape:randomized="0" inkscape:rounded="0" inkscape:flatsided="true" sodipodi:arg2="1.0585539" sodipodi:arg1="0.53495507" sodipodi:r2="19.118814" sodipodi:r1="36.271721" sodipodi:cy="659.36267" sodipodi:cx="184.46677" sodipodi:sides="6" id="path4259-8-5" fill="#353535" stroke="#000000" style="fill-opacity:1;stroke-width:0.9899115;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" sodipodi:type="star"></path>
             <text xml:space="preserve" style="font-style:normal;font-weight:normal;font-size:40px;line-height:125%;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" x="389.62216" y="768.37518" id="text5109-9" sodipodi:linespacing="125%"><tspan sodipodi:role="line" id="tspan5111-0" x="389.62216" y="768.37518" fill="#ffffff" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:condensed;font-size:40px;font-family:'agency-fb';-inkscape-font-specification:'Agency FB Bold Condensed';text-align:center;text-anchor:middle;fill-opacity:1">CASE</tspan></text>
@@ -148,4 +157,5 @@
 </div>
 </div>
 </div>
-       
+</div>
+</div>
