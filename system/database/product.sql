0978114923201e96ad1ecf74106ff8f074955096
PheeHandy <PheeHandy@gmail.com>
2018-12-31 21:20:17 +0700

    Update product.sql

--- system/database/product.sql
+++ system/database/product.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 31, 2018 at 02:49 PM
+-- Generation Time: Dec 31, 2018 at 03:19 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
